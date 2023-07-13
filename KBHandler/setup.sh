56418ab8bb3e34fe2e3b4d20a07f6cfe59ddc84f
Kanwal Arora <kanwalarora029@gmail.com>
2023-07-13 14:37:49 +0100

    Updated KB Handler

--- KBHandler/setup.sh
+++ KBHandler/setup.sh
@@ -1,5 +1,4 @@
-## get elasticsearch Image
-docker pull elasticsearch:7.14.2
-
-## run the container
-docker run -d -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.14.2
+# The first docker command pulls the elasticsearch image from the docker hub
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.14.2
+# We are running elasticsearch docker and mapping the 9200 port of local server with the host server
+docker run -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.14.2
