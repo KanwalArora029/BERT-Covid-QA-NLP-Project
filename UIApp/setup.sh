d1b5f9e8d5bc7c109296ddf4f340998764f402ff
Kanwal Arora <kanwalarora029@gmail.com>
2023-07-13 15:43:01 +0100

    Updated docker file

--- UIApp/setup.sh
+++ UIApp/setup.sh
@@ -1,10 +1,10 @@
 version=`date "+%H-%M-%S_%d-%m-%y"`
 echo $version
 
-sudo docker container prune -f
-sudo docker image prune -f
-sudo docker volume prune -f
-sudo docker network prune -f
+docker container prune -f
+docker image prune -f
+docker volume prune -f
+docker network prune -f
 
-sudo docker build -t qa-ui .
-sudo docker run -p 80:8080 qa-ui
+docker build -t qa-ui .
+docker run -p 80:8080 qa-ui
