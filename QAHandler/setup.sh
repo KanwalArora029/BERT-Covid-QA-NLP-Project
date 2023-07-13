5030507981a0835c10c3331a1ecf0b245ce4a60f
Kanwal Arora <kanwalarora029@gmail.com>
2023-07-13 19:45:31 +0100

    Final Docker changes

--- QAHandler/setup.sh
+++ QAHandler/setup.sh
@@ -10,4 +10,5 @@ docker network prune -f
 
 docker push us.gcr.io/annular-mercury-318319/qa-api:$version
 
-docker run -p 85:8080 qa-api -e ELASTIC_SEARCH_HOST='host.docker.internal' -e ELASTIC_SEARCH_PORT=9200
+##docker run -p 85:8080 qa-api -e ELASTIC_SEARCH_HOST='host.docker.internal' -e ELASTIC_SEARCH_PORT=9200
+docker run -p 85:8080 -e es_host='0.0.0.0' -e es_port=9200 qa-api
